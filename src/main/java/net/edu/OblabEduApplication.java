3279a30ac5169a9c8a65528e3559d84db9a15783
SHYANG-ENVY\shyang.oblab <shyang.oblab@gmail.com>
2017-04-24 15:31:42 +0900

    spring boot 프로젝트 생성

---
+++ src/main/java/net/edu/OblabEduApplication.java
@@ -0,0 +1,12 @@
+package net.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OblabEduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OblabEduApplication.class, args);
+	}
+}
---
+++ src/test/java/net/edu/OblabEduApplicationTests.java
@@ -0,0 +1,16 @@
+package net.edu;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OblabEduApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
